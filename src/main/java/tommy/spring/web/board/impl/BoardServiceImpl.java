0c919eb1a6221fda00d67720c3533bfcd4902abb
YongHyunBaek <slayers_yh@nate.com>
2022-07-15 13:44:54 +0900

    spring AOP

--- src/main/java/tommy/spring/web/board/impl/BoardServiceImpl.java
+++ src/main/java/tommy/spring/web/board/impl/BoardServiceImpl.java
@@ -1,34 +1,41 @@
 package tommy.spring.web.board.impl;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import tommy.spring.web.board.BoardService;
 import tommy.spring.web.board.BoardVO;
 @Service("boardService") //비즈니스 로직 처리를 위해 어노테이션 추가 
 public class BoardServiceImpl implements BoardService {
 	@Autowired
 	private BoardDAO boardDAO;
+	public BoardServiceImpl() {
+	
+	}
 	@Override
 	public void insertBoard(BoardVO vo) {
-		boardDAO.insertBoard(vo);
+//			if(vo.getSeq() == 0) {
+//				throw new IllegalArgumentException("0번 글은 등록할 수 없습니다.");
+//			}
+			boardDAO.insertBoard(vo);
 	}
 	@Override
 	public void updateBoard(BoardVO vo) {
-		boardDAO.updateBoard(vo);
+			
+			boardDAO.updateBoard(vo);
 	}
 	@Override
 	public void deleteBoard(BoardVO vo) {
-		boardDAO.deleteBoard(vo);
+			
+			boardDAO.deleteBoard(vo);
 	}
 	@Override
 	public BoardVO getBoard(BoardVO vo) {
+			
 			return boardDAO.getBoard(vo);
 	}
 	@Override
 	public List<BoardVO> getBoardList(BoardVO vo) {
+		
 			return boardDAO.getBoardList(vo);
 	}
 
